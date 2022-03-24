a860f65788105afe50a238534bb46827a231ba70
agrc <agrc14@gmail.com>
2022-03-24 22:20:00 +0100

    fix: Fix example

--- examples/size.rs
+++ examples/size.rs
@@ -1,20 +1,24 @@
 fn main() {
-  use einstein::SizeReport.
+  use einstein::SizeReport;
+  use std::collections::HashMap;
 
   // Let's check how much memory does a Hashmap<String, Vec<usize>> use.
-  let mut map = std::collections::Hashmap::new();
+  let mut map = HashMap::new();
   
   // Start adding new stuff to it.
   for i in 0..10 {
     let key = format!("{}", i);
     let mut val = Vec::new();
     
-    for j in 0..i {
+    for _ in 0..i {
       val.push(0usize)
     }
     
     map.insert(key, val);
   }
   
-  println!("Size of the hashmap:\n{}", map.fullsize());
+  println!("Size of the hashmap: {} bytes", map.fullsize());
+  println!("Direct (pointer + size + capacity + misc): {} bytes", HashMap::<String, Vec<usize>>::direct());
+  println!("Indirect (Actual size of keys and values): {} bytes", map.indirect());
+  println!("Indirect (Size collected of all children): {} bytes", map.children());
 }
